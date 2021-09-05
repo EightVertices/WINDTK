09f374c6539508018a7c28e34b6afb5165e00086
Renan Penha Marquette <rpenhamarquette@gmail.com>
2021-09-05 20:22:16 -0300

    New file organization

---
+++ WINDTK/WXN/Objects.cs
@@ -0,0 +1,28 @@
+﻿namespace WINDTK.WXN
+{
+    public class WXNPureObject
+    {
+        public string identifier;
+        public dynamic data;
+
+        public WXNPureObject(string identifier, dynamic data)
+        {
+            this.identifier = identifier;
+            this.data = data;
+        }
+    }
+
+    public class WXNObject : WXNPureObject
+    {
+        public WXNTypes type;
+        internal bool isArray;
+
+        public WXNObject(WXNTypes type, string identifier, object data) : base(identifier, data)
+        {
+            this.data = data;
+            this.type = type;
+            this.identifier = identifier;
+            isArray = type.ToString().Contains("Array");
+        }
+    }
+}
